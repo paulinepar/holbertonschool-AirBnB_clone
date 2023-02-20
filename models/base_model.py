c5f0cd68becd89126b8865c18b39f73c48642324
paulinepar <paulineparmigiani@gmail.com>
2023-02-20 15:28:39 +0100

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -4,24 +4,27 @@
     common attributes/methods for other classes:
 '''
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     '''generate class BaseModel'''
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.now())
-        self.updated_at = str(datetime.datetime.now())
+        self.created_at = self.get_time_iso()
+        self.updated_at = self.get_time_iso()
+
+    def get_time_iso(self):
+        return str(datetime.isoformat(datetime.now()))
 
     def __str__(self):
         '''function str'''
-        clas = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        clas = "[{}] ({}) {}".format(type(self).__name__, self.id, self.to_dict())
         return clas
 
     def save(self):
         '''function that update public instance'''
-        self.updated_at = str(datetime.datetime.now())
+        self.updated_at = self.get_time_iso()
 
     def to_dict(self):
         '''create dictionnary'''
