e8adb0c5d954d18d6fd8d4f30637b4feb2925297
= <hugo.chilemme@gmail.com>
2023-02-22 15:27:59 +0100

    Fix issue create model for all objects

--- main_8.py
+++ main_8.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import os
+from datetime import datetime
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 fs = FileStorage()
+print(type(fs.reload))
 file_path = "file.json"
 try:
     file_path = FileStorage._FileStorage__file_path
@@ -17,14 +19,19 @@ try:
     fs._FileStorage__objects.clear()
 except:
     pass
+
 ids = []
-objs_by_id = {}
 for i in range(10):
     bm = BaseModel()
+    bm.updated_at = datetime.utcnow()
     fs.new(bm)
-    bm.save()
     ids.append(bm.id)
-    objs_by_id[bm.id] = bm
+
+try:
+    os.remove(file_path)
+except:
+    pass
+fs.save()
 
 try:
     fs._FileStorage__objects.clear()
@@ -40,19 +47,6 @@ if len(all_reloaded.keys()) != len(ids):
 for id in ids:
     if all_reloaded.get(id) is None and all_reloaded.get("{}.{}".format("BaseModel", id)) is None:
         print("Missing {}".format(id))
-
-for id in ids:
-    obj_reloaded = all_reloaded.get(id)
-    if obj_reloaded is None:
-        obj_reloaded = all_reloaded.get("{}.{}".format("BaseModel", id))
-    obj_created = objs_by_id[id]
-
-    print(obj_reloaded.created_at, obj_created.created_at)
-    print(obj_reloaded.updated_at, obj_created.updated_at)
-    
-    # print(obj_reloaded.id == obj_created.id)
-    # print(obj_reloaded.created_at == obj_created.created_at)
-    # print(obj_reloaded.updated_at == obj_created.updated_at)
 
 try:
     os.remove(file_path)
