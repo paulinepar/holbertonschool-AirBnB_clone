20106658c0b4b7b1413d94001226210150db2fe1
Valentin <5619@holbertonstudents.com>
2023-02-22 00:30:00 -0800

    import and call storage.new

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 '''
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -14,6 +15,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
         else:
             self.update(kwargs)
 
@@ -33,6 +35,7 @@ class BaseModel:
     def save(self):
         '''function that update public instance'''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         '''create dictionnary'''
