c915a0576415bbd46e1203d8ccd314121d69abd1
Valentin <5619@holbertonstudents.com>
2023-02-23 07:03:45 -0800

    push base model

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,16 @@ from models import storage
 class BaseModel:
     '''generate class BaseModel'''
     def __init__(self, *args, **kwargs):
-        if not kwargs:
+        
+        if not kwargs or type(kwargs) is not dict:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
         else:
             self.update(kwargs)
+        
+
 
 
     def update(self, kwargs):
@@ -30,17 +33,20 @@ class BaseModel:
 
     def __str__(self):
         '''function str'''
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.to_dict())
+        d = self.to_dict().copy();
+        return "[{}] ({}) {}".format(BaseModel.__name__, self.id, d)
 
     def save(self):
         '''function that update public instance'''
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         '''create dictionnary'''
         dictionnary = self.__dict__.copy()
-        dictionnary['__class__'] = type(self).__name__
+
+        dictionnary['__class__'] = "BaseModel"
         dictionnary['created_at'] = self.created_at.isoformat()
         dictionnary['updated_at'] = self.updated_at.isoformat()
         return dictionnary
