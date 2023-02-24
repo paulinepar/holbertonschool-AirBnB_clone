ac6a14c72b25f0d4236e41e312ef085a81b78d9d
paulinepar <paulineparmigiani@gmail.com>
2023-02-24 18:04:57 +0100

    test amenity

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -7,5 +7,10 @@ from models.amenity import Amenity
 class Test_Amenity(unittest.TestCase):
     '''class Test'''
     def test_Name(self):
+        '''test if is right name'''
         ame = Amenity(name="sauna")
         self.assertEqual(ame.name, "sauna")
+
+    def test_str(self):
+        '''test if is str'''
+        self.assertEqual(str, type(Amenity.name))
